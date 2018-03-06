f83b7216e20524f729c4aedc2667829b97f89138
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-03-06 19:06:03 +0100

    The game choose a random word from the txt file

--- Hangman.java
+++ Hangman.java
@@ -1,31 +1,49 @@
 import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.Random;
+import java.io.IOException;
 
 public class Hangman {
-    static Scanner userInput = new Scanner(System.in);
+  static Scanner userInput = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        int chosenMenu = displayMenu();
-        switch (chosenMenu) {
-        case 1:
-            game();
-            break;
-        case 2:
-            return;
-        }
+  public static void main(String[] args) {
+    int chosenMenu = displayMenu();
+    switch(chosenMenu){
+      case 1:
+        game();
+        break;
+      case 2:
+        return;
+    }
+  }
+  private static int displayMenu(){
+    System.out.println("Welcome in Hangman game!");
+    System.out.println("Choose a menupoint:");
+    System.out.println("1. Game");
+    System.out.println("2. Quit");
+    int chosenMenuPoint = userInput.nextInt();
+    return chosenMenuPoint;
+  }
+  private static void game(){
+    String pickedWord = pickWord("dictionary.txt");
+  }
+  private static void displayLeaderboards(){
+  }
+  private static String pickWord(String fileName) {
+    try {
+      Stream<String> words = Files.lines(Paths.get(fileName));
+      String[] result = words.toArray(String[]::new);
+      int idx = new Random().nextInt(result.length);
+      String pickedWord = (result[idx]);
+      return pickedWord;
+    } catch(IOException e) {
+      return "";
     }
 
-    private static int displayMenu() {
-        System.out.println("Welcome in Hangman game!");
-        System.out.println("Choose a menupoint:");
-        System.out.println("1. Game");
-        System.out.println("2. Quit");
-        int chosenMenuPoint = userInput.nextInt();
-        return chosenMenuPoint;
-    }
-
-    private static void game() {
-    }
-
-    private static void displayLeaderboards() {
-    }
+  }
 }
