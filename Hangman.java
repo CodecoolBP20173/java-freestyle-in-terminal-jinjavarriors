39ab0a71124f1ed124bec31d08bb0399e8be2582
Tamás Varjas <tamas.varjas00@gmail.com>
2018-03-07 09:51:49 +0100

    Created new game object when running starts and created arrays to store correct and wrong characters

--- Hangman.java
+++ Hangman.java
@@ -9,41 +9,54 @@ import java.util.Random;
 import java.io.IOException;
 
 public class Hangman {
-  static Scanner userInput = new Scanner(System.in);
+    char[] wrongChars = new char[7];
+    char[] correctChars;
+    String pickedWord;
+    static Scanner userInput = new Scanner(System.in);
 
-  public static void main(String[] args) {
-    int chosenMenu = displayMenu();
-    switch(chosenMenu){
-      case 1:
-        game();
-        break;
-      case 2:
-        return;
-    }
-  }
-  private static int displayMenu(){
-    System.out.println("Welcome in Hangman game!");
-    System.out.println("Choose a menupoint:");
-    System.out.println("1. Game");
-    System.out.println("2. Quit");
-    int chosenMenuPoint = userInput.nextInt();
-    return chosenMenuPoint;
-  }
-  private static void game(){
-    String pickedWord = pickWord("dictionary.txt");
-  }
-  private static void displayLeaderboards(){
-  }
-  private static String pickWord(String fileName) {
-    try {
-      Stream<String> words = Files.lines(Paths.get(fileName));
-      String[] result = words.toArray(String[]::new);
-      int idx = new Random().nextInt(result.length);
-      String pickedWord = (result[idx]);
-      return pickedWord;
-    } catch(IOException e) {
-      return "";
+    public static void main(String[] args) {
+        int chosenMenu = displayMenu();
+        switch (chosenMenu) {
+        case 1:
+            game();
+            break;
+        case 2:
+            return;
+        }
     }
 
-  }
+    private static int displayMenu() {
+        System.out.println("Welcome in Hangman game!");
+        System.out.println("Choose a menupoint:");
+        System.out.println("1. Game");
+        System.out.println("2. Quit");
+        int chosenMenuPoint = userInput.nextInt();
+        return chosenMenuPoint;
+    }
+
+    private static void game() {
+        Hangman game = new Hangman();
+        game.pickedWord = pickWord("dictionary.txt");
+        game.correctChars = new char[game.pickedWord.length()];
+        System.out.println(game.pickedWord);
+        System.out.println(Arrays.toString(game.correctChars));
+        System.out.println(Arrays.toString(game.wrongChars));
+    }
+
+    private static void displayLeaderboards() {
+    }
+
+    private static String pickWord(String fileName) {
+        try {
+            Stream<String> words = Files.lines(Paths.get(fileName));
+            String[] result = words.toArray(String[]::new);
+            int idx = new Random().nextInt(result.length);
+            String pickedWord = (result[idx]);
+            return pickedWord;
+        } catch (IOException e) {
+            return "";
+        }
+
+    }
+ 
 }
