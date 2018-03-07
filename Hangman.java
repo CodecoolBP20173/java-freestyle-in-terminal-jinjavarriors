8c47ac62496ca387a7be41e1d8e29c407bf8f3ad
Tamás Varjas <tamas.varjas00@gmail.com>
2018-03-07 13:31:31 +0100

    Filled correctChars array with underscores

--- Hangman.java
+++ Hangman.java
@@ -40,6 +40,9 @@ public class Hangman {
         Hangman game = new Hangman();
         game.pickedWord = pickWord("dictionary.txt");
         game.correctChars = new char[game.pickedWord.length()];
+        for (int i = 0; i < game.correctChars.length; i++) {
+            game.correctChars[i] = "_";
+        }
         game.tries = 7;
         //System.out.println(game.pickedWord);
         while (true) {
