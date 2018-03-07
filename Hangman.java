96eef03ed24fe1032409dec9393f946278a92f44
Tamás Varjas <tamas.varjas00@gmail.com>
2018-03-07 10:48:16 +0100

    Checking input and adding the character to correct or wrong char array

--- Hangman.java
+++ Hangman.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 import java.util.Arrays;
 import java.util.Random;
 import java.io.IOException;
+import java.lang.*;
 
 public class Hangman {
     char[] wrongChars = new char[7];
@@ -38,9 +39,10 @@ public class Hangman {
         Hangman game = new Hangman();
         game.pickedWord = pickWord("dictionary.txt");
         game.correctChars = new char[game.pickedWord.length()];
-        System.out.println(game.pickedWord);
-        System.out.println(Arrays.toString(game.correctChars));
-        System.out.println(Arrays.toString(game.wrongChars));
+        //System.out.println(game.pickedWord);
+        check_char_input("a", game.pickedWord, game.correctChars, game.wrongChars);
+        //System.out.println(Arrays.toString(game.correctChars));
+        //System.out.println(Arrays.toString(game.wrongChars));
     }
 
     private static void displayLeaderboards() {
@@ -57,6 +59,31 @@ public class Hangman {
             return "";
         }
 
+    }
+
+    private static void check_char_input(String input, String pickedWord, char[] correctChars, char[] wrongChars) {
+        if (input.length() > 1) {
+            if (input == pickedWord) {
+                System.out.println("YES!!!");
+            } else {
+                System.out.println("NO!!!");
+            }
+        } else {
+            if (pickedWord.contains(input)) {
+                for (int i = 0; i < pickedWord.length(); i++) {
+                    if (pickedWord.charAt(i) == input.charAt(0)) {
+                        correctChars[i] = input.charAt(0);
+                    }
+                }
+            } else {
+                for (int i = 0; i < wrongChars.length; i++) {
+                    if (wrongChars[i] == '\u0000') {
+                        wrongChars[i] = input.charAt(0);
+                        break;
+                    }
+                }
+            }
+        }
     }
  
 }
